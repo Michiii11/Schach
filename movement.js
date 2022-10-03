a64f80fcd039571611e2330451be3bd923ccba71
Michiii <michael.leisch@gmx.at>
2022-10-03 20:01:27 +0200

    PreMove

--- movement.js
+++ movement.js
@@ -4,11 +4,31 @@ let figure; // Current Figure
 let picked = false; // Status if figure is picked
 let white = true; // Current Player
 
+/**
+ * Moves the figure in the gameMatrix
+ * then sets the Figures swap the move and remove the Mark
+ * @param {*} position Position to move
+ * @param {*} figure Figure to move
+ */
 function moveFigure(position, figure) {
     gameMatrix[position[1]][position[0]] = figure;
+    gameMatrix[y1][x1] = 0;
+    setFigures();
+    swapMove();
+    removeMark();
 }
 
-function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position after
+/**
+ * Checks the move if it is possible
+ * @param {*} figure Figure
+ * @param {*} x1 x-Coordinate from the figure
+ * @param {*} y1 y-Coordinate from the figure
+ * @param {*} x2 x-Coordinate to check
+ * @param {*} y2 y-Coordinate to check
+ * @returns true or false, if the move is valid
+ */
+function checkMove(figure, x1, y1, x2, y2) {
+     // Checks if the Endposition isn't a own figure
     if(gameMatrix[y2][x2] != 0){
         if(gameMatrix[y2][x2].substr(gameMatrix[y2][x2].length-1, 1) == figure.substr(figure.length-1, 1)){
             return false
@@ -18,7 +38,7 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
     if (figure == "pawnW") {
         // Check if Farmer didn't move before
         if (x1 == x2) { // Move is in one Line
-            if (y1 == 1) {
+            if (y1 == 1) { // Checks if it's the first move
                 if (y2 == y1 + 2 || y2 == y1 + 1 && gameMatrix[y2][x2] == 0) {
                     if(y2 == y1 + 2 && gameMatrix[y1+1][x1] != 0){
                         return false;
@@ -33,7 +53,7 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
                 }
                 return false
             }
-        } else{
+        } else{ // Crash a figure
             if(x2==x1-1 || x2==x1+1){
                 if(y2 == y1+1 && gameMatrix[y2][x2] != 0){
                     return true;
@@ -44,7 +64,7 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
     } else if (figure == "pawnB") {
         // Check if Farmer didn't move before
         if (x1 == x2) { // Move is in one Line
-            if (y1 == 6) {
+            if (y1 == 6) { // Checks if it's the first move
                 if (y2 == y1 - 2 || y2 == y1 - 1 && gameMatrix[y2][x2] == 0) {
                     if(y2 == y1 - 2 && gameMatrix[y1-1][x1] != 0){
                         return false;
@@ -59,7 +79,7 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
                 }
                 return false
             }
-        } else{
+        } else{ // Crash a figure
             if(x2==x1-1 || x2==x1+1){
                 if(y2 == y1-1 && gameMatrix[y2][x2] != 0){
                     return true;
@@ -71,13 +91,17 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
 
     let tfigure = figure.substr(0, figure.length - 1);
     if (tfigure == "knight") {
+        // Difference between the coordinates
         let x = x1-x2;
         let y = y1-y2;
+
+        // Possible moves
         let knight = [
             {"x": -2, "y": 1}, {"x": -1, "y": 2}, {"x": 1, "y": 2}, {"x": 2, "y": 1}, 
             {"x": 2, "y": -1}, {"x": 1, "y": -2}, {"x": -1, "y": -2}, {"x": 2, "y": -1}
         ]
 
+        // Checks if the move is one of the possible moves
         for (let i = 0; i < knight.length; i++) {
             if(knight[i].x == x && knight[i].y == y) {
                 return true
@@ -101,10 +125,8 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
             return true
         }
     } else if (tfigure == "queen") {
-        // Check diagonal oder gerade , wenn gerade dann rook, wenn diagonl dann bishop
-        console.log(Math.abs(x2-x1) == Math.abs(y2-y1), Math.abs(x2-x1)!=0 || Math.abs(x2-x1)!=0)
-        console.log(Math.abs(x2-x1), Math.abs(y2-y1))
-        if(Math.abs(x2-x1) == Math.abs(y2-y1)){
+        // Check diagonal or linear, if linear rook, if diagonal bishop
+        if(Math.abs(x2-x1) == Math.abs(y2-y1)){ // Diagonal
             let xD;
             let yD;
             if(x1 > x2){xD = -1;} 
@@ -118,7 +140,7 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
                 }
             }
             return true
-        } else if(Math.abs(x2-x1)!=0 && y2-y1==0 || Math.abs(y2-y1)!=0 && x2-x1==0){
+        } else if(Math.abs(x2-x1)!=0 && y2-y1==0 || Math.abs(y2-y1)!=0 && x2-x1==0){ // Linear
             let diff;
             if(x2 != x1){
                 diff = Math.abs(x2-x1)
@@ -148,35 +170,39 @@ function checkMove(figure, x1, y1, x2, y2) { // Figur, Position before, Position
         }
         return false;
     } else if (tfigure == "king") {
-        // King not next to King
+        // King not next to King (Opposition)
         let king = [
             {"x": -1, "y": -1}, {"x": -1, "y": 0}, {"x": -1, "y": 1},
             {"x": 0, "y": -1}, {"x": 0, "y": 0}, {"x": 0, "y": 1},
             {"x": 1, "y": -1}, {"x": 1, "y": 0}, {"x": 1, "y": 1},
         ]
+
         for (let i = 0; i < king.length; i++) {
-            if(figure == tfigure + "W") {
-                if(gameMatrix[y2+king[i].y][x2+king[i].x] == tfigure+"B") {
-                    return false;
+            if(x2+king[i].x >= 0 && x2+king[i].x < 8 && y2+king[i].y >= 0 && y2+king[i].y < 8) {
+                if(figure == tfigure + "W") {
+                    if(gameMatrix[y2+king[i].y][x2+king[i].x] == tfigure+"B") {
+                        return false;
+                    }
                 }
-            }
-            if(figure == tfigure + "B") {
-                if(gameMatrix[y2+king[i].y][x2+king[i].x] == tfigure+"W") {
-                    return false;
+                if(figure == tfigure + "B") {
+                    if(gameMatrix[y2+king[i].y][x2+king[i].x] == tfigure+"W") {
+                        return false;
+                    }
                 }
             }
         }
 
         // Standard Movement
-        if(Math.abs(x1-x2) == 1 || Math.abs(y1-y2) == 1) {
+        let diffX = Math.abs(x1-x2)
+        let diffY = Math.abs(y1-y2)
+        if(diffX == 1 && diffY == 0 || diffX == 0 && diffY == 1 || diffX == 1 && diffY == 1 || diffX == 1 && diffY == 1) {
             return true
         }
 
         // Rochade
-        console.log(document.querySelector(`.F${x1}${y1}`).classList)
+/*         console.log(document.querySelector(`.F${x1}${y1}`).classList)
         if(document.querySelector(`.F${x1}${y1}`).classList)
-
-
+ */
         return false
     } else if (tfigure == "rook") {
         if(x2-x1 == 0 || y2-y1 == 0){
--- script.js
+++ script.js
@@ -11,9 +11,13 @@ let gameMatrix = [
 ]
 
 
-//----------- Build Chess Board -----------//
+//----------- Build -----------//
 let side = false;
 buildChessboard();
+/**
+ * Builds the Chessboard in HTML
+ * You can swap the sides
+ */
 function buildChessboard() { // true == white , false == black
     let content = ""
 
@@ -49,67 +53,14 @@ function buildChessboard() { // true == white , false == black
         side = true;
     }
     document.getElementById("chessboard").innerHTML = content;
-    setMuster();
+    setPattern();
     setFigures();
 }
 
-//----------- Tools -----------//
-let prevPos = null;
-function pickFigure(position) {
-    let x = position[0]
-    let y = position[1]
-
-    if(prevPos && gameMatrix[y][x] != 0){
-        if(prevPos != position){
-            if(white == true && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "W" || white == false && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "B"){
-                picked = false;
-                prevPos = null;
-                removeMark(position)
-            }
-        }
-    }
-
-    if (picked == false || prevPos != position) {
-        if (!prevPos && gameMatrix[y][x] != "") {
-            if(white == true && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "W" || white == false && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "B"){
-                if (document.querySelector('.active')) {
-                    removeMark();
-                }
-                addMark(position)
-                picked = true;
-                prevPos = position
-            }
-        } else{
-            if(prevPos){
-                x1 = parseInt(prevPos.substr(0,1))
-                y1 = parseInt(prevPos.substr(1,1))
-                let check = checkMove(gameMatrix[y1][x1], x1, y1, parseInt(x), parseInt(y))
-                if(check){
-                    document.querySelector(`.F${prevPos[0]}${prevPos[1]}`).classList.remove("notUsed")
-
-                    let pos = [x, y]
-                    moveFigure(pos, gameMatrix[y1][x1])
-                    gameMatrix[y1][x1] = 0;
-                    setFigures();
-    
-                    swapMove();
-                    picked = false;
-                    prevPos = null;
-                    removeMark();
-                }
-            }
-        }
-    } else {
-        picked = false;
-        prevPos = null;
-        removeMark(position)
-    }
-}
-
+/**
+ * Set Figure is a method which goes throw the Game Matrix and sets the Figures
+ */
 function setFigures() {
-    let startN = 1;
-    let startL = 'a';
-
     for (let x = 0; x < 8; x++) {
         for (let y = 7; y >= 0; y--) {
             document.querySelector(`.F${x}${y}`).style.backgroundImage = "none";
@@ -120,7 +71,10 @@ function setFigures() {
     }
 }
 
-function setMuster() {
+/**
+ * Set Template is a method which draws the Chessboard pattern
+ */
+function setPattern() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             if ((i + j) % 2 == 0) {
@@ -129,13 +83,139 @@ function setMuster() {
         }
     }
 }
-function getPos(x, y) {
+
+/**
+ * @param {*} x Coordinate
+ * @param {*} y Coordinate
+ * @returns the number of the box
+ */
+ function getPos(x, y) {
     let basepos = 0
     if (y > 0)
         basepos = y * 8
     return document.querySelectorAll('.box')[basepos + x]
 }
 
+
+//----------- Tools -----------//
+let prevPos = null;
+/**
+ * Picks a figure and checks if it can be activated to mark or move
+ * @param {*} position Array with [x][y] coordinates
+ */
+function pickFigure(position) {
+    let x = position[0]
+    let y = position[1]
+
+    if(prevPos && gameMatrix[y][x] != 0){ // If a other figure get picked it get marked
+        if(prevPos != position){
+            if(white == true && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "W" || white == false && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "B"){
+                picked = false;
+                prevPos = null;
+                removeMark(position)
+            }
+        }
+    }
+
+    if (picked == false || prevPos != position) { // If a figure is already picked and the position is the same as previous the marker gets removed
+        if (!prevPos && gameMatrix[y][x] != "") { // Mark the Figure
+            if(white == true && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "W" || white == false && gameMatrix[y][x].substr(gameMatrix[y][x].length - 1, 1) == "B"){
+                if (document.querySelector('.active')) {
+                    removeMark();
+                }
+                preMove(x,y)
+                addMark(position)
+                picked = true;
+                prevPos = position
+            }
+        } else{ // Make a move / check the move
+            if(prevPos){
+                x1 = parseInt(prevPos.substr(0,1))
+                y1 = parseInt(prevPos.substr(1,1))
+                let check = checkMove(gameMatrix[y1][x1], x1, y1, parseInt(x), parseInt(y))
+                if(check){ // Make the move
+                    document.querySelector(`.F${prevPos[0]}${prevPos[1]}`).classList.remove("notUsed")
+
+                    let pos = [x, y]
+                    moveFigure(pos, gameMatrix[y1][x1])
+                    
+                    if(checkCheck()){
+                        let check = checkCheck();
+                        checkCheckmate(check)
+                    }
+
+                    picked = false;
+                    prevPos = null;
+                }
+            }
+        }
+    } else { // Remove marker
+        picked = false;
+        prevPos = null;
+        removeMark(position)
+    }
+}
+
+/**
+ * Checks if the king is checked
+ */
+function checkCheck(){
+    // Get King Position
+    let xW, yW;
+    let xB, yB
+    for (let x = 0; x < 8; x++) {
+        for (let y = 0; y < 8; y++) {
+            if(gameMatrix[y][x] == "kingW"){
+                yW = y
+                xW = x
+            } else if(gameMatrix[y][x] == "kingB"){
+                yB = y
+                xB = x
+            }
+        }
+    }   
+
+    for (let x0 = 0; x0 < 8; x0++) {
+        for (let y0 = 0; y0 < 8; y0++) {
+            figure = gameMatrix[y0][x0]
+            if(figure != 0){
+                if(checkMove(figure, x0, y0, xW, yW)){
+                    console.log(figure, x0, y0, xW, yW, "White")
+                    return "W";
+                }
+                if(checkMove(figure, x0, y0, xB, yB)){
+                    console.log(figure, x0, y0, xB, yB, "Black")
+                    return "B";
+                }
+            }
+        }
+    }
+    return false;
+}
+
+/**
+ * Checks if the king is checkmate
+ * @returns true or false
+ */
+function checkCheckmate(check){
+    let position = getPosFigure("king"+check)
+    position = position[0]
+    let x = position[0]
+    let y = position[1]
+
+    for (let x0 = 0; x0 < 8; x0++) {
+        for (let y0 = 0; y0 < 8; y0++) {
+            if(checkMove("king"+check, x, y, x0, y0)){
+                console.log(x0, y0)
+            }
+        }
+    }
+    return true;
+}
+
+/**
+ * Swaps the Move from white to black or black to white
+ */
 function swapMove(){
     if(white == false){
         white = true;
@@ -145,10 +225,73 @@ function swapMove(){
         document.querySelector('#who').innerHTML = "Schwarz ist am Zug!"
     }
 }
+
+/**
+ * Adds a marker at a position
+ * @param {*} position which should be marked
+ */
 function addMark(position){
     let elem = document.querySelector(`.F${position}`)
     elem.classList.add("active")
 }
+
+/**
+ * Removes all marker
+ */
 function removeMark(){
     document.querySelector('.active').classList.remove("active");
+    removePreMove();
+}
+
+function getPosFigure(figure){
+    let positions = new Array(8)
+    for (let i = 0; i < positions.length; i++) {
+        positions[i] = new Array(2);
+    }
+
+    let temp = 0;
+    for (let x = 0; x < 8; x++) {
+        for (let y = 0; y < 8; y++) {
+            if(gameMatrix[y][x] == figure){
+                positions[temp][0] = x;
+                positions[temp][1] = y;
+                temp++;
+            }
+        }
+    }
+    return positions;
+}
+
+
+//----------- Features -----------//
+/**
+ * Gets the possible moves of the activated figure
+ * and sets the preMove image into the HTML
+ */
+function preMove(x,y){
+    x = parseInt(x); y = parseInt(y)
+    figure = gameMatrix[y][x]
+
+    for (let x0 = 0; x0 < 8; x0++) {
+        for (let y0 = 0; y0 < 8; y0++) {
+            if(checkMove(figure, x, y, x0, y0)){
+                document.querySelector(`.F${x0}${y0}`).innerHTML = "<img src='./Pictures/preMove.png'>"
+            }
+        }
+    }
+}
+
+/**
+ * Removes all preMove images
+ */
+function removePreMove(){
+    for (let x = 0; x < 8; x++) {
+        for (let y = 0; y < 8; y++) {
+            document.querySelector(`.F${x}${y}`).innerHTML = ""
+        }
+    }
+}
+
+function endMatch(){
+    console.log("End")
 }
